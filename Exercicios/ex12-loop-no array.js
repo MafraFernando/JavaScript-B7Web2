45ee7ba5a804542c8a7cfc21075e20b755637c29
Fernando Mafra <145868730+MafraFernando@users.noreply.github.com>
2024-07-31 01:32:30 -0300

    atualizado ex de loop

---
+++ Exercicios/ex12-loop-no array.js
@@ -0,0 +1,9 @@
+for (let n = 0; n < 10; n++) {
+    console.log('Item n ' + n);
+};
+
+let cores = ['branco', 'preto', 'marrom', 'azul', 'vermelho', 'verde'];
+
+for (let n = 0; cores.length; n++) { //o loop só roda a quantidade de vezes que o array tem
+    console.log(cores[n]);
+}
